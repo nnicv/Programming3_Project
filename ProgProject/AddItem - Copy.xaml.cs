20cd190a4434566e394bc24e0b7f0c67e26f0d1a
kevinlaskai <60619425+kevinlaskai@users.noreply.github.com>
2020-12-18 19:19:07 -0500

    Added Xmal

---
+++ ProgProject/AddItem - Copy.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgProject
+{
+    /// <summary>
+    /// Interaction logic for AddItem.xaml
+    /// </summary>
+    public partial class AddItem : Window
+    {
+        public AddItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProgProject/UpdateItem.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgProject
+{
+    /// <summary>
+    /// Interaction logic for UpdateItem.xaml
+    /// </summary>
+    public partial class UpdateItem : Window
+    {
+        public UpdateItem()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
